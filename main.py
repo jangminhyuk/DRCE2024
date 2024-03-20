58ba735bade90c817593b3edaf51a5495bdd5e9a
jangminhyuk <jason4012@snu.ac.kr>
2024-03-21 01:18:11 +0900

    21MAR24

--- main.py
+++ main.py
@@ -114,14 +114,14 @@ def main(dist, noise_dist1, num_sim, num_samples, num_noise_samples, T, plot_res
     B = C = Q = R = Qf = np.eye(10) 
     #----------------------------
     # change True to False if you don't want to use given lambda
-    use_lambda = True
+    use_lambda = False
     lambda_ = 15 # will not be used if the parameter "use_lambda = False"
     noisedist = [noise_dist1]
     #theta_v_list  # radius of noise ambiguity set
     #theta_w_list  # theta_w have no effect if the parameter "use_lambda = True"
     if dist == "normal":
-        theta_w_list = [1.5]
-        theta_v_list = [1.5]
+        theta_w_list = [1.0]
+        theta_v_list = [4.0]
     elif dist == "quadratic":
         theta_w_list = [1.0]
         theta_v_list = [4.0]
@@ -182,8 +182,8 @@ def main(dist, noise_dist1, num_sim, num_samples, num_noise_samples, T, plot_res
                     if noise_dist =="normal":
                         v_max = None
                         v_min = None
-                        M = 1.5*np.eye(ny) #observation noise covariance
-                        mu_v = -1.0*np.ones((ny, 1))
+                        M = 2.0*np.eye(ny) #observation noise covariance
+                        mu_v = 1.0*np.ones((ny, 1))
                     elif noise_dist =="quadratic":
                         v_min = -2.5*np.ones(ny)
                         v_max = 0.0*np.ones(ny)
--- main_param.py
+++ main_param.py
@@ -135,8 +135,8 @@ def main(dist, noise_dist1, num_sim, num_samples, num_noise_samples, T, plot_res
                         #disturbance distribution parameters
                         w_max = None
                         w_min = None
-                        mu_w = 0.1*np.ones((nx, 1))
-                        Sigma_w= 0.1*np.eye(nx)
+                        mu_w = 0.2*np.ones((nx, 1))
+                        Sigma_w= 0.2*np.eye(nx)
                         #initial state distribution parameters
                         x0_max = None
                         x0_min = None
@@ -144,13 +144,13 @@ def main(dist, noise_dist1, num_sim, num_samples, num_noise_samples, T, plot_res
                         x0_cov = 0.1*np.eye(nx)
                     elif dist == "quadratic":
                         #disturbance distribution parameters
-                        w_max = 0.2*np.ones(nx)
-                        w_min = -0.1*np.ones(nx)
+                        w_max = 0.5*np.ones(nx)
+                        w_min = 0.0*np.ones(nx)
                         mu_w = (0.5*(w_max + w_min))[..., np.newaxis]
                         Sigma_w = 3.0/20.0*np.diag((w_max - w_min)**2)
                         #initial state distribution parameters
-                        x0_max = 0.1*np.ones(nx)
-                        x0_min = -0.1*np.ones(nx)
+                        x0_max = 0.5*np.ones(nx)
+                        x0_min = 0.0*np.ones(nx)
                         x0_mean = (0.5*(x0_max + x0_min))[..., np.newaxis]
                         x0_cov = 3.0/20.0 *np.diag((x0_max - x0_min)**2)
                     elif dist =="uniform":
@@ -169,11 +169,11 @@ def main(dist, noise_dist1, num_sim, num_samples, num_noise_samples, T, plot_res
                     if noise_dist =="normal":
                         v_max = None
                         v_min = None
-                        M = 2.5*np.eye(ny) #observation noise covariance
-                        mu_v = 0.2*np.ones((ny, 1))
+                        M = 2.0*np.eye(ny) #observation noise covariance
+                        mu_v = 1.0*np.ones((ny, 1))
                     elif noise_dist =="quadratic":
-                        v_min = -2.0*np.ones(ny)
-                        v_max = 2.0*np.ones(ny)
+                        v_min = -2.5*np.ones(ny)
+                        v_max = 0*np.ones(ny)
                         mu_v = (0.5*(v_max + v_min))[..., np.newaxis]
                         M = 3.0/20.0 *np.diag((v_max-v_min)**2) #observation noise covariance
                     elif noise_dist == "uniform":
