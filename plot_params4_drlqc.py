9fbe61daf7b6c20b550a2a21499221ddf6b1ef02
jangminhyuk <jason4012@snu.ac.kr>
2024-05-13 00:32:10 +0900

    13MAY24

--- plot_params4_drlqc.py
+++ plot_params4_drlqc.py
@@ -255,8 +255,8 @@ if __name__ == "__main__":
     
     theta_v_list = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0] # radius of noise ambiguity set
     #theta_v_list = [0.5, 1.0, 2.0, 3.0, 4.0, 5.0]
-    theta_v_list = [1.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0]
-    theta_v_list = [5.0, 10.0, 15.0]
+    theta_v_list = [1.0, 2.0, 3.0, 4.0, 5.0]
+    #theta_v_list = [5.0, 10.0, 15.0]
     theta_w_list = [1.0, 2.0, 3.0, 4.0, 5.0]
     if args.dist=='normal':
         lambda_list = [12, 15, 20, 25, 30, 35, 40, 45, 50] # disturbance distribution penalty parameter
