9655bd962d86dec0251637324ba83471678df744
minhyukjang <jason4012@snu.ac.kr>
2024-05-18 01:54:21 +0900

    18MAY24

--- plot_params4_F.py
+++ plot_params4_F.py
@@ -60,7 +60,7 @@ def summarize_lambda(wdrc_drkf_lambda_values, wdrc_drkf_theta_v_values, wdrc_drk
     #ax.scatter(wdrc_lambda_values, wdrc_theta_values, wdrc_cost_values, label='WDRC')
 
     # Plot smooth surface - WDRC
-    surface_wdrc =ax.plot_surface(lambda_grid_wdrc, theta_v_grid_wdrc, cost_grid_wdrc, alpha=0.6, color='blue', label='WDRC')
+    surface_wdrc =ax.plot_surface(lambda_grid_wdrc, theta_v_grid_wdrc, cost_grid_wdrc, alpha=0.5, color='blue', label='WDRC')
     surfaces.append(surface_wdrc)
     labels.append('WDRC [3]')
     #--------------
@@ -77,7 +77,7 @@ def summarize_lambda(wdrc_drkf_lambda_values, wdrc_drkf_theta_v_values, wdrc_drk
     )
     
     # Plot smooth surface - DCE
-    surface_wdrc_drkf = ax.plot_surface(lambda_grid_wdrc_drkf, theta_v_grid_wdrc_drkf, cost_grid_wdrc_drkf, alpha=0.5, color='red', label='WDRC+DRKF')
+    surface_wdrc_drkf = ax.plot_surface(lambda_grid_wdrc_drkf, theta_v_grid_wdrc_drkf, cost_grid_wdrc_drkf, alpha=0.4, color='red', label='WDRC+DRKF')
     surfaces.append(surface_wdrc_drkf)
     labels.append('WDRC + DRKF [54]')
     #--------------
@@ -94,7 +94,7 @@ def summarize_lambda(wdrc_drkf_lambda_values, wdrc_drkf_theta_v_values, wdrc_drk
     )
     
     # Plot smooth surface - DCE
-    surface_drcmmse = ax.plot_surface(lambda_grid_drcmmse, theta_v_grid_drcmmse, cost_grid_drcmmse, alpha=0.6, color='gold', label='WDRC+DRMMSE', antialiased=False)
+    surface_drcmmse = ax.plot_surface(lambda_grid_drcmmse, theta_v_grid_drcmmse, cost_grid_drcmmse, alpha=0.6, color='silver', label='WDRC+DRMMSE')
     surfaces.append(surface_drcmmse)
     labels.append('WDRC + DRMMSE [55]')
     
